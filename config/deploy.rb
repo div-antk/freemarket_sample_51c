3169180ed5498f9d93698cef395d1f47e1ccb89b
div-antk <51283468+owaii@users.noreply.github.com>
2019-08-12 19:14:58 +0900

    FIX AWS-AccessKey

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,6 +34,8 @@ set :default_env, {
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
   BASIC_AUTH_USER: ENV["BASIC_AUTH_USER"],
   BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"]
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
 
 # Default branch is :master
